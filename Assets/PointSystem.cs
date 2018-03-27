e8088a7744a8022435c2d6ef22791e731aeac22e
morimotoryou <ryo@winryo.local>
2018-03-28 05:05:38 +0900

    PinBallのコミット
    まだ、UIの表示がおかしいのですが、課題を提出いたします。確認、ご教授よろしく御願いいたします。

---
+++ Assets/PointSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointSystem : MonoBehaviour {
+	 float point;
+	 float sumPoints;
+	//pointコンポーネントを入れる
+	 private GameObject pointsText;
+
+	// Use this for initialization
+	void Start () {
+		//unity上のpointオブジェクトを取得
+		this.pointsText = GameObject.Find ("Point");
+	}
+	
+
+
+	void OnCollisionEnter(Collision abc){
+
+		if (abc.gameObject.tag == "LargeStarTag") {
+			point = 10;
+		} else if (abc.gameObject.tag == "SmallCloudTag") {
+			point = 20;
+		} else if (abc.gameObject.tag == "LargeCloudTag") {
+			point = 15;
+		} else {
+			point = 0;
+		}
+
+		sumPoints += point;
+		Debug.Log (sumPoints,this);
+	}
+
+//	 Update is called once per frame
+	void Update () {
+
+		this.pointsText.GetComponent<Text> ().text = sumPoints.ToString();
+
+	}
+
+}
+
